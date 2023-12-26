1cb504b5f9ab2666f4485b9f6e58d1830b395139
kTrzcinskii <kt.trzcinski@gmail.com>
2023-12-26 17:34:51 +0100

    Backend init

---
+++ git-logs-server/src/main/java/dev/ktrzcinski/gitlogsserver/GitLogsServerApplication.java
@@ -0,0 +1,13 @@
+package dev.ktrzcinski.gitlogsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLogsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitLogsServerApplication.class, args);
+    }
+
+}
---
+++ git-logs-server/src/test/java/dev/ktrzcinski/gitlogsserver/GitLogsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ktrzcinski.gitlogsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLogsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
