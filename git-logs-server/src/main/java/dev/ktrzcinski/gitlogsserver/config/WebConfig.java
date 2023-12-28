da9bb99c8e04d8237985c4e56692646dec10a457
kTrzcinskii <kt.trzcinski@gmail.com>
2023-12-28 21:33:11 +0100

    Handle CORS

---
+++ git-logs-server/src/main/java/dev/ktrzcinski/gitlogsserver/config/WebConfig.java
@@ -0,0 +1,17 @@
+package dev.ktrzcinski.gitlogsserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Origin", "Content-Type", "Accept")
+                .maxAge(60 * 60 * 24);
+    }
+}
